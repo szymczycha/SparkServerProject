99a688f3449a37fc72d8a11a1f61572cf7f54812
szymczycha <szymczycha266@gmail.com>
2022-10-17 09:54:10 +0200

    L02?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import static spark.Spark.*;
 import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.UUID;
 
 import static spark.Spark.externalStaticFileLocation;
@@ -20,14 +21,46 @@ public class App {
         post("/add", (req,res) -> add(req,res,cars));
         get("/json", (req,res) -> jsonCars(req,res,cars));
         post("/delete", (req,res) -> deleteCar(req,res,cars));
-//        post("/update", ...)
+        post("/update", (req,res) -> updateCar(req,res,cars));
+        post("/generateRandom", (req,res) -> generateCars(req,res,cars));
     }
-
+    private static String generateCars(Request req, Response res, ArrayList<Car> cars){
+        for (int i = 0; i<20; i++){
+            cars.add(new Car(cars.size()+1));
+        }
+        return "{success:\"tak\"";
+    }
+    private static String updateCar(Request req, Response res, ArrayList<Car> cars) {
+        Gson gson = new Gson();
+        System.out.println("update");
+        System.out.println(req.body());
+//        Car newCar = gson.fromJson(req.body(), Car.class);
+        String newCarModel = gson.fromJson(req.body(), Car.class).getModel();
+        UUID newCarUUID = gson.fromJson(req.body(), Car.class).getUuid();
+        int newCarYear = gson.fromJson(req.body(), Car.class).getYear();
+        System.out.println(newCarUUID);
+        System.out.println(newCarModel);
+        System.out.println(newCarYear);
+        for(Car car : cars){
+            System.out.println(car.getUuid());
+            if(car.getUuid().equals(newCarUUID)){
+                car.setModel(newCarModel);
+                car.setYear(newCarYear);
+            }
+        }
+        return "{pog: \"pog\"}";
+    }
     private static String deleteCar(Request req, Response res, ArrayList<Car> cars) {
         Gson gson = new Gson();
-        UUID uuidToDelete = gson.fromJson(req.body(), UUID.class);
+        System.out.println(gson.fromJson(req.body(), Car.class));
+        UUID uuidToDelete = gson.fromJson(req.body(), Car.class).getUuid();
+//        System.out.println(uuidToDelete);
         for(Car car : cars){
-            if(car.getUuid() == uuidToDelete){
+//            System.out.println(car.getUuid());
+//            System.out.println(uuidToDelete==car.getUuid());
+//            System.out.println(uuidToDelete.equals(car.getUuid()));
+            if(car.getUuid().equals(uuidToDelete)){
+//                System.out.println(car);
                 cars.remove(car);
                 break;
             }
@@ -64,6 +97,7 @@ class Car{
     private String model;
     private int year;
     private String color;
+    private boolean hasInvoice;
     private ArrayList<Airbag> airbags;
 
     public int getId() {
@@ -80,6 +114,38 @@ class Car{
 
     public int getYear() {
         return year;
+    }
+
+    public void setHasInvoice(boolean hasInvoice) {
+        this.hasInvoice = hasInvoice;
+    }
+
+    public boolean isHasInvoice() {
+        return hasInvoice;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setAirbags(ArrayList<Airbag> airbags) {
+        this.airbags = airbags;
     }
 
     public String getColor() {
@@ -101,7 +167,21 @@ class Car{
                 ", airbags=" + airbags +
                 '}';
     }
+    public Car(int id){
+        this.id = id;
+        UUID uuid = Generators.randomBasedGenerator().generate();
+        this.airbags = new ArrayList<>();
+        this.airbags.add(new Airbag("pasażer", false));
+        this.airbags.add(new Airbag("kierowca", false));
+        this.airbags.add(new Airbag("tylna kanapa", false));
+        this.airbags.add(new Airbag("boczne z tylu", false));
+        Random random = new Random();
+        int nextInt = random.nextInt(0xffffff + 1);
+        this.color = String.format("#%06x", nextInt);
+        this.year = 1970 + random.nextInt(52);
 
+
+    }
     public Car(int id, String model, int year, String color, ArrayList<Airbag> airbags) {
         this.id = id;
         UUID uuid = Generators.randomBasedGenerator().generate();
@@ -110,6 +190,7 @@ class Car{
         this.year = year;
         this.color = color;
         this.airbags = airbags;
+        this.hasInvoice = false;
     }
 
     public Car() {
@@ -118,6 +199,11 @@ class Car{
 class Airbag{
     private String description;
     private boolean value;
+
+    public Airbag(String description, boolean value) {
+        this.description = description;
+        this.value = value;
+    }
 
     @Override
     public String toString() {
